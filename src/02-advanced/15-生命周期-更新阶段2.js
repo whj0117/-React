eb7b7ca24d0a83457afc1e329316516684509e94
whj0117 <791281470@qq.com>
2022-05-12 20:38:41 +0800

    生命周期shouldComponentUpdate

---
+++ src/02-advanced/15-生命周期-更新阶段2.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: whj0117
+ * @Date: 2022-05-12 20:25:31
+ * @Github: https://github.com/whj0117
+ * @Email: 791281470@qq.com
+ * @LastEditors: whj0117
+ */
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    state = {
+        myname: '小明'
+    }
+
+    componentDidUpdate() { console.log('componentDidUpdate') }
+
+    shouldComponentUpdate(nextProps,nextState){
+        //return true 应该更新
+        //return false 阻止更新
+        console.log(nextProps,nextState)
+        return !(nextState.myname === this.state.myname)
+    }
+    
+    render() {
+        return (
+            <div>
+                <button onClick={() => {
+                    this.setState({
+                        myname: '小明1'
+                    })
+                }}>click</button>
+                {this.state.myname}
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './02-advanced/14-生命周期-更新阶段1';
+import App from './02-advanced/15-生命周期-更新阶段2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
