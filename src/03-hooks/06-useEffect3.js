19ed7315ee8d00c5ed94c9b3b89c7f88dd3c65d2
王弘剑 <791281470@qq.com>
2022-05-23 00:06:23 +0800

    useEffect和useLayoutEffect区别备注

--- src/03-hooks/06-useEffect3.js
+++ src/03-hooks/06-useEffect3.js
@@ -6,27 +6,11 @@
  * @Email: 791281470@qq.com
  */
 
+// useEffect和useLayoutEffect有什么区别？
+// 简单来说就是调用时机不同，useLayoutEffect和原来componentDidMount & componentDidUpdate一致，在react完成DOM更新后马上同步调用的代码，会阻塞页面渲染。而useEffect是会在整个页面渲染完成才会调用的代码。
+// 官方建议优先用useEffect
+// 在实际使用时如果想避免页面抖动（在useEffect里修改DOM很有可能出现）的话，可以需要操作DOM的代码放在useLayoutEffect里。在这里做点dom操作，这些dom修改会和react做出的更改一起被一次性渲染到屏幕上，只有一次回流、重绘的代价。
 import React, { Component, useEffect } from 'react'
-
-// class Child extends Component {
-//     componentDidMount() {
-//         window.onresize = () => {
-//             console.log('onresize')
-//         }
-//         this.timer = setInterval(()=>{
-//             console.log('setInterval')
-//         },1000)
-//     }
-//     componentWillUnmount(){
-//         window.onresize = null;
-//         this.timer && clearInterval(this.timer)
-//     }
-//     render() {
-//         return <div>
-//             Child
-//         </div>
-//     }
-// }
 
 function Child(){
     useEffect(()=>{
